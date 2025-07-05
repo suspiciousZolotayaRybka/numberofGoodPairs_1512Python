5039f7d666bb25c6265d411fa52820b40ca84014
Isaac Finehout <finehout.isaac@gmail.com>
2025-07-05 15:56:03 -0600

    Initial Python commit with numberofGoodPairs_1512 05Jun25155

---
+++ main.py
@@ -0,0 +1,42 @@
+'''
+/**
+ * Isaac Finehout
+ * 07/05/2025
+ * Leetcode
+ * NumberofGoodPairs_1512
+ * https://leetcode.com/problems/number-of-good-pairs/description/
+ *
+ */
+//Given an array of integers nums, return the number of good pairs.
+//
+//A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,2,3,1,1,3]
+//Output: 4
+//Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+//Example 2:
+//
+//Input: nums = [1,1,1,1]
+//Output: 6
+//Explanation: Each pair in the array are good.
+//Example 3:
+//
+//Input: nums = [1,2,3]
+//Output: 0
+//
+//
+//Constraints:
+//
+//1 <= nums.length <= 100
+//1 <= nums[i] <= 100
+'''
+import numberofGoodPairs_1512
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    solution = numberofGoodPairs_1512.numberofGoodPairs_1512()
+    print(solution.numIdenticalPairs([0, 0, 0]))
